d1df24a3efbb566f63707a6d52f2246c7cec1f38
Jordan Bonecutter <jordanbonecutter@gmail.com>
2020-08-11 15:45:02 -0700

    hid callable functions from user so they dont use them

--- memtools.h
+++ memtools.h
@@ -12,20 +12,8 @@
 
 #ifndef MEMTOOLS_H_INCLUDE_GUARD
   #define MEMTOOLS_H_INCLUDE_GUARD
-  #include <stdbool.h>
-  #include <stdlib.h>
-
-void* memtools_malloc (size_t n, unsigned int line, char* file);/* Version of malloc which keeps track of line & file where memory was allocated */
-void  memtools_free(void* ptr, unsigned int line, char* file); /* Version of free which keeps track of line and file where memory was deallocated */
-void* memtools_realloc(void* ptr, size_t n, unsigned int line, char* file); /* Version of realloc which keeps track of line and file where memory was reallocated */
-
-void memtools_print_allocated(); /* print all currently allocated memory */
-bool memtools_has_memory_been_violated(void* ptr); /* check if any over allocated segments are corrupted */
-bool memtools_memory_comment(void* ptr, char* comment); /* add comment to memory */
-bool memtools_is_valid_pointer(void* ptr); /* check if pointer is valid */
-
   #ifdef MEMTOOLS
-    #include <stdio.h>
+    #include "memtools_internal.h"
 
     #define malloc(n)     memtools_malloc (n, __LINE__, __FILE__)
     #define free(p)       memtools_free   (p, __LINE__, __FILE__)
